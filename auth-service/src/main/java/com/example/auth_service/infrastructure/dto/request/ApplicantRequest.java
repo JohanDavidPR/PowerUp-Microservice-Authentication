4a56004a730d5ab5e0218e5e4f735135b45cfa51
Johan <prd5546@gmail.com>
2025-08-24 23:38:24 -0500

    feat: se agrega dto para recibir info del aplicante

---
+++ auth-service/src/main/java/com/example/auth_service/infrastructure/dto/request/ApplicantRequest.java
@@ -0,0 +1,29 @@
+package com.example.auth_service.infrastructure.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class ApplicantRequest {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    private String dateOfBirth;
+
+    private String address;
+    private String phoneNumber;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotNull
+    @Min(0)
+    @Max(15000000)
+    private Double baseSalary;
+}
