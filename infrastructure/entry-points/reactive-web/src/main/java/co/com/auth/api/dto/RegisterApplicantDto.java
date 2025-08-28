a6df25c8a3bf46c8e0eb3c496e95e847c5443a24
Johan <prd5546@gmail.com>
2025-08-27 19:47:36 -0500

    feat: se agrega manejo de excepcion global y se integran Dtos al sistema

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/auth/api/dto/RegisterApplicantDto.java
@@ -0,0 +1,17 @@
+package co.com.auth.api.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class RegisterApplicantDto {
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String address;
+    private String phone;
+    private String email;
+    private BigDecimal baseSalary;
+}
---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/auth/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package co.com.auth.api.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> errorResponse = Map.of("error", ex.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+}
