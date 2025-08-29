434a00f00d9afb0a96b4d07302baf23ffd1f165a
Johan <prd5546@gmail.com>
2025-08-28 22:50:15 -0500

    feat: se agregan test unitarios de caso de uso

--- applications/app-service/src/main/java/co/com/auth/MainApplication.java
+++ applications/app-service/src/main/java/co/com/auth/MainApplication.java
@@ -1,5 +1,7 @@
 package co.com.auth;
 
+import co.com.auth.api.logging.ReactiveLogger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -8,6 +10,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 public class MainApplication {
     public static void main(String[] args) {
+        ReactiveLogger.enable(LoggerFactory.getLogger("reactive-trace"));
         SpringApplication.run(MainApplication.class, args);
     }
 }
