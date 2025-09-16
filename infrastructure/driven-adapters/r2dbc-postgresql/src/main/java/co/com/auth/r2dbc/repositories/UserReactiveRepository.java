2a1ce29d5035c160c3dca7ddbefef2d9c2f492ee
J. David Portocarrero <prd5546@gmail.com>
2025-09-16 17:08:20 -0500

    Se ajusta metodos de intentos en repository

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/auth/r2dbc/repositories/UserReactiveRepository.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/auth/r2dbc/repositories/UserReactiveRepository.java
@@ -2,13 +2,15 @@ package co.com.auth.r2dbc.repositories;
 
 import co.com.auth.model.user.User;
 import co.com.auth.r2dbc.entity.UserEntity;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface UserReactiveRepository extends ReactiveCrudRepository<UserEntity, String>, ReactiveQueryByExampleExecutor<UserEntity> {
-    Mono<User> login(String email, String password);
     Mono<User> findByEmail(String email);
+    @Query("SELECT attempts FROM user WHERE email = :email")
     Mono<Integer> getAttempts(String email);
+    @Query("UPDATE user SET attempts = attempts + 1 WHERE email = :email")
     Mono<Void> incrementAttempts(String email);
 }
