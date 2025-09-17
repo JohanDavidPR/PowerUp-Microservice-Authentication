1858e1929feb08fda1875bb710cf131a2e0deb8c
Johan <prd5546@gmail.com>
2025-09-17 12:11:42 -0500

    Se agregan metodo a interfaz y caso de uso para guardar usuario

--- domain/model/src/main/java/co/com/auth/model/user/gateways/UserRepository.java
+++ domain/model/src/main/java/co/com/auth/model/user/gateways/UserRepository.java
@@ -8,4 +8,5 @@ public interface UserRepository {
     Mono<Integer> getAttempts(String email);
     Mono<Void> incrementAttempts(String email);
     Mono<Boolean> validatePassword(String password, String encodedPassword);
+    Mono<User> save(User user);
 }
--- domain/usecase/src/main/java/co/com/auth/usecase/registeruser/RegisterUserUseCase.java
+++ domain/usecase/src/main/java/co/com/auth/usecase/registeruser/RegisterUserUseCase.java
@@ -1,6 +1,15 @@
 package co.com.auth.usecase.registeruser;
 
+import co.com.auth.model.user.User;
+import co.com.auth.model.user.gateways.UserRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
 @RequiredArgsConstructor
 public class RegisterUserUseCase {
+    private final UserRepository userRepository;
+
+    public Mono<Void> registerUser(User user) {
+        return userRepository.save(user).then();
+    }
 }
